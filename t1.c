999e1c8e26e1ad32250067e85be6267ee2cf9f62
marcelo <marcelobenesciutti@gmail.com>
2017-11-08 12:00:45 -0200

    sequencial

--- t1.c
+++ t1.c
@@ -4,8 +4,10 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <string.h>
+#include <math.h>
 
 char buffer[50];
+float minddp;
 
 char *get_rec(FILE *file){
 	int i = 0, j;
@@ -23,29 +25,66 @@ char *get_rec(FILE *file){
   return buffer;
 }
 
-int sequencial(int *matriz){
-	
+float findmin(int linhateste, int nlinhasbase, int natributos, float matrizteste[][natributos], float matrizbase[][natributos]){
+  int i, j;
+  float soma, minlinha;
+  minlinha = 9999999999999;
+  for (i = 0; i < nlinhasbase; i++){
+    for (j = 0; j < natributos; j++){
+      soma += powf((matrizteste[linhateste][j] - matrizbase[i][j]), 2);
+    }
+    soma = sqrtf(soma);
+    if(soma < minlinha)
+      minlinha = soma;
+    }
+  
+  return minlinha;
+}
+
+void sequencial(int nlinhasteste, int nlinhasbase, int natributos, float matrizteste[][natributos], float matrizbase[][natributos]){
+  int i, j, k, l;
+  float soma = 0;
+  for (i = 0; i < nlinhasteste; i++){
+    minddp = findmin(i, nlinhasbase, natributos, matrizteste, matrizbase);
+    printf ("linha %i %f\n",i,minddp);
+  }            
+}
+
+void paralelizado(int nlinhasteste, int nlinhasbase, int natributos, float matrizteste[][natributos], float matrizbase[][natributos]){
+  
 }
 	
 int main (int argc, char *argv[]){
   int i, j;
   float atributoi;
-  int natributos, nlinhas;
+  int natributos, nlinhasbase, nlinhasteste;
   char *atributo = malloc(sizeof(char)*50);
-  char *nomearquivo = (char*)malloc(20 * sizeof(char));
+  char *nomearquivobase = (char*)malloc(20 * sizeof(char));
+  char *nomearquivoteste = (char*)malloc(20 * sizeof(char));
  	FILE *ARQUIVOBASE;
-  nomearquivo = argv[1]; 
-  ARQUIVOBASE = fopen(nomearquivo, "r");
-  natributos = atoi(argv[2]);
-  nlinhas = atoi(argv[3]); 
-  float base[nlinhas][natributos];
-  for (i = 0; i < nlinhas; i++){
+  FILE *ARQUIVOTESTE;
+  nomearquivobase = argv[1];
+  nomearquivoteste = argv[2];
+  ARQUIVOBASE = fopen(nomearquivobase, "r");
+  ARQUIVOTESTE = fopen(nomearquivoteste, "r");
+  natributos = atoi(argv[3]);
+  nlinhasteste = atoi(argv[4]);
+  nlinhasbase = atoi(argv[5]);
+  float base[nlinhasbase][natributos];
+  float teste[nlinhasteste][natributos];
+  for (i = 0; i < nlinhasbase; i++){
     for (j = 0; j < natributos; j++){
       atributo = get_rec(ARQUIVOBASE);
       atributoi = atof(atributo);
-      base[i][j] = atributoi;    
+      base[i][j] = atributoi;
     } 
-   //get_rec(ARQUIVOBASE);
   }
-  printf ("Primeiro elemento linha 2: %f \n", base[1][0]);
-}
+  for (i = 0; i < nlinhasteste; i++){
+    for (j = 0; j < natributos; j++){
+      atributo = get_rec(ARQUIVOTESTE);
+      atributoi = atof(atributo);
+      teste[i][j] = atributoi;
+    }
+  }
+  sequencial(nlinhasteste, nlinhasbase, natributos, teste, base);
+} 
